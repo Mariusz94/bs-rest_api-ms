164b7f3a1fde9bc48a5f781b687ccfd3769d5e23
MariuszLyszczarzAigo <mariusz.lyszczarz@aigorithmics.com>
2023-09-13 02:10:31 +0200

    refactor

--- docs/conf.py
+++ docs/conf.py
@@ -5,34 +5,40 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-import os,sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../app')))
-sys.path.append(r"../app/grpcfile")
+import os, sys
 
-project = 'bs-rest_api-ms'
-copyright = '2023, Dream team'
-author = 'Dream team'
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../app")))
+sys.path.append(r"../app/grpc_file")
+
+project = "bs-rest_api-ms"
+copyright = "2023, Dream team"
+author = "Dream team"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.todo","sphinx.ext.viewcode","sphinx.ext.autodoc","sphinx.ext.napoleon"]
+extensions = [
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
 latex_elements = {
-    'papersize': 'a4paper',         # Format papieru (np. a4paper, letterpaper)
-    'classoptions': ',oneside',    # Opcje klasy dokumentu, 'oneside' to jednostronny dokument
-    'preamble': r'''
+    "papersize": "a4paper",  # Format papieru (np. a4paper, letterpaper)
+    "classoptions": ",oneside",  # Opcje klasy dokumentu, 'oneside' to jednostronny dokument
+    "preamble": r"""
       \usepackage[columns=1]{idxlayout}
-    ''' # 1 kolumna w index
+    """,  # 1 kolumna w index
 }
 
+latex_documents = [("index", "doc.tex", project, author, "manual")]
--- docs/index.rst
+++ docs/index.rst
@@ -1,9 +1,9 @@
-.. bs-rest_api-ms documentation master file, created by
+.. python documentation master file, created by
    sphinx-quickstart on Sat Jul 29 19:37:03 2023.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to bs-rest_api-ms's documentation!
+Welcome to python's documentation!
 =====================================================
 
 .. toctree::
@@ -18,3 +18,4 @@ Indices and tables
 * :ref:`genindex`
 * :ref:`modindex`
 * :ref:`search`
+
